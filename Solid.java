db4432991ef445885ca362ce81fd6d6675a3c6bf
hj <tntj123@naver.com>
2023-05-09 19:55:12 +0900

    first commit : SOLID

---
+++ Solid.java
@@ -0,0 +1,25 @@
+public class Solid {
+
+    public static void main(String[] args) {
+        /*
+
+            SOLID
+
+            1. SRP - 단일 책임의 원칙
+
+            2. OCP - 개방 폐쇠 원칙
+                code를 개방에는 open, 수정에는 close.
+                예) 기능 보완시(수정) 기존의 클래스를 건들지 않고, 새로 class를 생성하여 기능을 보안
+
+            3. LSP - 리스코브 치환의 원칙
+
+            4. ISP - 인터페이스 분리의 원칙
+
+            5. DIP - 의존성 역전 원칙
+                추상화에 의존, 구체적인 것에는 의존하지 않는다.
+
+
+     */
+
+    }
+}
